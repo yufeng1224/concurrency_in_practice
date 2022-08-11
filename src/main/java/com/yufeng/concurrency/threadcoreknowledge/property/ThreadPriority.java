25a5f9564d485339dd3e07dfe923182570581f75
俞峰 <450409563@qq.com>
2022-08-11 17:22:53 +0800

    线程优先级演示代码提交

---
+++ src/main/java/com/yufeng/concurrency/threadcoreknowledge/property/ThreadPriority.java
@@ -0,0 +1,32 @@
+package com.yufeng.concurrency.threadcoreknowledge.property;
+
+/**
+ * @description
+ *      线程优先级测试
+ * @author yufeng
+ * @create 2020-02-23
+ */
+public class ThreadPriority {
+
+    public static void main(String[] args) {
+        Runnable task1 = () -> {
+            int count = 0;
+            for (;;) {
+                System.out.println("---->1 " + count++);
+            }
+        };
+        Runnable task2 = () -> {
+            int count = 0;
+            for (;;) {
+//                Thread.yield();
+                System.out.println("              ---->2 " + count++);
+            }
+        };
+        Thread t1 = new Thread(task1, "t1");
+        Thread t2 = new Thread(task2, "t2");
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t2.setPriority(Thread.MAX_PRIORITY);
+        t1.start();
+        t2.start();
+    }
+}
